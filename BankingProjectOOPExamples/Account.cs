71f9410de25e6311b6a0e821c8a856018e53db26
Alex Chan <AJChan303@gmail.com>
2019-07-29 13:38:51 -0400

    initial commit added class "Account" and tested it

---
+++ BankingProjectOOPExamples/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingProjectOOPExamples {
+    public class Account {
+
+        public string Number { get; set; }
+        private decimal Balance { get; set; } = 0.0M; // M = decimal
+        public string Name { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        public void Deposit(decimal Amount) {
+            if (Amount <= 0) {
+                Console.WriteLine("Deposit amounts must be greater than zero");
+            }
+            else {
+                this.Balance += Amount;
+            }
+        }
+        public void Withdraw(decimal Amount) {
+            if (Amount <= 0) {
+                Console.WriteLine("Withdraw amount must be GT zero");
+            } else {
+                if (this.Balance >= Amount) {
+                    this.Balance -= Amount;
+                }
+            }
+        }
+        public decimal GetBalance() {
+            return this.Balance;
+        }
+    }
+    }
--- BankingProjectOOPExamples/Program.cs
+++ BankingProjectOOPExamples/Program.cs
@@ -3,7 +3,25 @@
 namespace BankingProjectOOPExamples {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Account acct = new Account();
+            acct.Number = "1001";
+            acct.Name = "Test Account";
+            acct.Deposit(20);
+            acct.Withdraw(10);
+            decimal balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+            acct.Deposit(-10);
+             balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+            acct.Withdraw(-10);
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+            acct.Withdraw(20);
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+
+
+
         }
     }
 }
